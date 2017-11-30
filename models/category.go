5783adfc9c8e22060104f56097ab2ba5bfdcf8cb
Rath Vong <rathvong@mekongmessenger.com>
2017-11-29 20:42:17 -0500

    update query

--- models/category.go
+++ models/category.go
@@ -69,7 +69,7 @@ func (c * Category) queryCreate() (qry string){
 							created_at,
 							updated_at)
 							VALUES
-							($1, $2, $3, $4, $5, $6, $7, $8, $9)
+							($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
 							RETURNING id
 							`
 }
