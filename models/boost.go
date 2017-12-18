17ac1551fa8ec7bf3b42d530142f80ecce26327e
Rath Vong <rathvong@mekongmessenger.com>
2017-12-18 12:59:10 -0500

    update build

--- models/boost.go
+++ models/boost.go
@@ -45,7 +45,7 @@ func (b *Boost) queryCreate() (qry string){
 						created_at,
 						updated_at)
 				VALUES
-						($1, $2, $3, $4, $5, $6)
+						($1, $2, $3, $4, $5, $6, $7)
 				RETURNING id`
 }
 
