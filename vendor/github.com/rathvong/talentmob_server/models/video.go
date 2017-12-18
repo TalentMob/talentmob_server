099d50ba790a1966ce99a3c5e2316f90a4f492b9
Rath Vong <rathvong@mekongmessenger.com>
2017-12-18 15:45:49 -0500

    update build

--- vendor/github.com/rathvong/talentmob_server/models/video.go
+++ vendor/github.com/rathvong/talentmob_server/models/video.go
@@ -672,13 +672,10 @@ func (v *Video) queryRecentVideos() (qry string){
 				return videos, err
 			}
 
-			if err = boost.GetByVideoID(db, video.ID); err != nil {
-				if err != sql.ErrNoRows {
-					return videos, err
-				}
-
+			if err = boost.GetByVideoID(db, video.ID); err != nil && err != sql.ErrNoRows {
+				return videos, err
 			}
-
+			
 			video.Boost = boost
 
 			video.Publisher = user
@@ -736,11 +733,8 @@ func (v *Video) parseQueryRows(db *system.DB, rows *sql.Rows, userID uint64, wee
 		}
 
 
-		if err = boost.GetByVideoID(db, video.ID); err != nil {
-			if err != sql.ErrNoRows {
+		if err = boost.GetByVideoID(db, video.ID); err != nil && err != sql.ErrNoRows {
 				return videos, err
-			}
-
 		}
 
 		video.Boost = boost
