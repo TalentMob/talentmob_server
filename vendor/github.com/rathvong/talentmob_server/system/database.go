42966aa21c3382aead53817cb9a734994dea9a74
Rath Vong <rathvong@mekongmessenger.com>
2018-02-13 22:09:18 +0700

    latest build

---
+++ vendor/github.com/rathvong/talentmob_server/system/database.go
@@ -0,0 +1,52 @@
+package system
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"sync"
+	"time"
+)
+
+// DB struct will be a global variable in main to handle all db calls
+type DB struct {
+	*sql.DB
+}
+
+var once sync.Once
+
+
+// connect to database with a url
+// url string - location of the database
+func Connect(url string) *DB {
+
+	var db *sql.DB
+
+	once.Do(func() {
+		db, _ = sql.Open("postgres", url)
+
+		db.SetMaxOpenConns(87) // Sane default
+		db.SetMaxIdleConns(0)
+		db.SetConnMaxLifetime(time.Nanosecond)
+
+		err := db.Ping()
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+
+	})
+
+
+	return &DB{db}
+}
+
+// Ping database for connection
+func (db *DB) PingConnectionToDatabase() {
+	log.Println("PingConnectionToDatabase()")
+	err := db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+}
