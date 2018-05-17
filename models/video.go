74559046cb82852cf084c62c9d89c75565233ab7
Rath Vong <rathvong@mekongmessenger.com>
2018-05-17 09:28:32 +0700

    update time-line order

--- models/video.go
+++ models/video.go
@@ -168,7 +168,7 @@ func (v *Video) queryTimeLine() (qry string){
 		AND videos.user_id != $1
 		AND videos.is_active = true
 		AND videos.upvote_trending_count IS NULL
-		ORDER BY  videos.user_id ASC
+		ORDER BY  videos.user_id DESC
 		LIMIT 100
 		OFFSET 0
  		) as v
