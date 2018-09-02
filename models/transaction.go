279b4a8b5643fb8b0d27b1cd8109175b4a263716
Rath Vong <rathvong@mekongmessenger.com>
2018-09-02 11:00:28 +0700

    update transaction

--- models/transaction.go
+++ models/transaction.go
@@ -126,7 +126,7 @@ func (t *Transaction) Create(db *system.DB) error {
 			created_at,
 			updated_at	
 			) VALUES (
-			$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12	
+			$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,$12, $13, $14	
 			) RETURNING id
 			`
 
